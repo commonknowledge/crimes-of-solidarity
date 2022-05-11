b3427f8a2e6dc4c175dad3791e6ba9fc1f710768
John Evans <38507954+King-Mob@users.noreply.github.com>
2022-05-11 17:26:31 +0100

    add error boundary component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,55 +1,74 @@
-import * as Fathom from 'fathom-client';
-import { DefaultSeo } from 'next-seo';
-import App from 'next/app';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { SWRConfig } from 'swr';
-import { KonamiCode } from '../components/KonamiCode';
-import { getMenuItems } from '../data/menuItem';
-import { useCanonicalURL } from '../data/seo';
-import { projectStrings } from '../data/site';
-import '../styles/globals.css';
-import { doNotFetch } from '../utils/swr';
+import * as Fathom from "fathom-client";
+import { DefaultSeo } from "next-seo";
+import App from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { SWRConfig } from "swr";
+import { KonamiCode } from "../components/KonamiCode";
+import { getMenuItems } from "../data/menuItem";
+import { useCanonicalURL } from "../data/seo";
+import { projectStrings } from "../data/site";
+import "../styles/globals.css";
+import { doNotFetch } from "../utils/swr";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 function MyApp({ Component, pageProps, headerLinks, footerLinks }) {
-  const canonicalURL = useCanonicalURL()
+  const canonicalURL = useCanonicalURL();
 
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
-    Fathom.load('OZSKHUQE', {
-      includedDomains: ['gameworkersolidarity.com', 'www.gameworkersolidarity.com'],
-      url: 'https://skunk.gameworkersolidarity.com/script.js',
-    })
+    Fathom.load("OZSKHUQE", {
+      includedDomains: [
+        "gameworkersolidarity.com",
+        "www.gameworkersolidarity.com",
+      ],
+      url: "https://skunk.gameworkersolidarity.com/script.js",
+    });
 
     function onRouteChangeComplete() {
-      Fathom.trackPageview()
+      Fathom.trackPageview();
     }
 
-
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
 
     return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
 
   return (
-    <SWRConfig value={{
-      initialData: { ...pageProps, headerLinks, footerLinks },
-      ...doNotFetch()
-    }}>
+    <SWRConfig
+      value={{
+        initialData: { ...pageProps, headerLinks, footerLinks },
+        ...doNotFetch(),
+      }}
+    >
       <DefaultSeo
         defaultTitle={projectStrings.name}
         titleTemplate={`%s | ${projectStrings.name}`}
         description={projectStrings.description}
         canonical={canonicalURL}
         additionalLinkTags={[
-          { rel: 'icon', href: '/favicon/favicon.ico' },
-          { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
-          { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon/favicon-32x32.png" },
-          { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon/favicon-16x16.png" },
-          { rel: "manifest", href: "/site.webmanifest" }
+          { rel: "icon", href: "/favicon/favicon.ico" },
+          {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/favicon/apple-touch-icon.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/favicon/favicon-32x32.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/favicon/favicon-16x16.png",
+          },
+          { rel: "manifest", href: "/site.webmanifest" },
         ]}
         openGraph={{
           url: canonicalURL,
@@ -58,33 +77,36 @@ function MyApp({ Component, pageProps, headerLinks, footerLinks }) {
           description: projectStrings.description,
           images: [
             {
-              url: projectStrings.baseUrl + `/images/game-workers-share-card.png`,
-              alt: 'Game Worker Solidarity',
-            }
-          ]
+              url:
+                projectStrings.baseUrl + `/images/game-workers-share-card.png`,
+              alt: "Game Worker Solidarity",
+            },
+          ],
         }}
         twitter={{
           handle: projectStrings.twitterHandle,
           site: projectStrings.twitterHandle,
-          cardType: 'summary_large_image',
+          cardType: "summary_large_image",
         }}
       />
       <div>
         <KonamiCode />
-        <Component {...pageProps} />
+        <ErrorBoundary>
+          <Component {...pageProps} />
+        </ErrorBoundary>
       </div>
     </SWRConfig>
-  )
+  );
 }
 
 MyApp.getInitialProps = async (appContext) => {
   const appProps = await App.getInitialProps(appContext);
-  const links = await getMenuItems()
+  const links = await getMenuItems();
   return {
     ...appProps,
-    headerLinks: links.filter(l => l.fields.placement.includes('Header')),
-    footerLinks: links.filter(l => l.fields.placement.includes('Footer'))
-  }
-}
+    headerLinks: links.filter((l) => l.fields.placement.includes("Header")),
+    footerLinks: links.filter((l) => l.fields.placement.includes("Footer")),
+  };
+};
 
-export default MyApp
+export default MyApp;
